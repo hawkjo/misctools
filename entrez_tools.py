f122b401b18e409c097a3d832ec2c67055baf9ce
John Hawkins <hawkjo@gmail.com>
2014-07-30 09:51:19 -0500

    Initial commit for entrez_tools

---
+++ entrez_tools.py
@@ -0,0 +1,39 @@
+import sys
+from Bio import Entrez
+from my_pprint import pprint_list
+
+Entrez.email = 'hawkjo@gmail.com'
+
+def list_dbs():
+    print Entrez.email
+    handle = Entrez.einfo()
+    record = Entrez.read(handle)
+
+    for k,v in record.items():
+        print k,':'
+        print
+        pprint_list(list(v))
+
+def list_searchable_fields(db):
+    record = Entrez.read( Entrez.einfo(db='protein') )
+
+    for field in record['DbInfo']['FieldList']:
+        print '%(Name)s, %(FullName)s, %(Description)s' % field
+
+def download_all_hits( db, search_term, outfname, batch_size = 10, outfmt = 'fasta' ):
+    search_results = Entrez.read( Entrez.esearch(db=db, term=search_term, usehistory='y') )
+    
+    count = int( search_results['Count'] )
+    webenv = search_results['WebEnv']
+    query_key = search_results['QueryKey']
+    
+    with open(outfname, 'w') as out:
+        for start in range(0,count,batch_size):
+            end = min(count, start+batch_size)
+            print('Downloading %s record %i to %i of %i' % (outfname, start+1, end, count))
+            fetch_handle = Entrez.efetch(db=db, rettype=outfmt, retmode='text',
+                                            retstart=start, retmax=batch_size,
+                                            webenv=webenv, query_key=query_key)
+            data = fetch_handle.read()
+            fetch_handle.close()
+            out.write(data)
