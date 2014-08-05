e92be4f761d217b548f850f1826390690284cbd2
John Hawkins <hawkjo@gmail.com>
2014-08-05 10:35:24 -0500

    Update to misc_tools gzip capabilities
    Renamed get_open_fnc to get_gzip_friendly_open_fnc
    Fixed bug in get_gzip_friendly_open_fnc
    Added gzip_friendly_open function

--- misc_tools.py
+++ misc_tools.py
@@ -1,6 +1,6 @@
-import sys, os
+import sys, os, gzip
 
-def get_open_fnc( *args ):
+def get_gzip_friendly_open_fnc( *args ):
     """
     get_open_fnc returns the correct open function to use for file, gzip compatible.
     Arguments:
@@ -8,7 +8,7 @@ def get_open_fnc( *args ):
     Returns:
         The open function to use: either 'open' or 'gzip.open'
     """
-    fname_extensions = set([os.splitext( fname ) for fname in args])
+    fname_extensions = set([os.path.splitext( fname )[-1] for fname in args])
     if '.gz' not in fname_extensions:
         return open
     elif fname_extensions == set(['.gz']):
@@ -16,3 +16,12 @@ def get_open_fnc( *args ):
         return gzip.open
     else:
         sys.exit('Error: All files must be of the same format and either fastq or gzipped fastq files.')
+
+def gzip_friendly_open( fname, mode='r' ):
+    """
+    gzip_friendly_open returns a file handle appropriate for the file, whether gzipped or not.
+    """
+    if os.path.splitext(fname)[-1] == '.gz':
+        return gzip.open(fname, mode)
+    else:
+        return open(fname, mode)
