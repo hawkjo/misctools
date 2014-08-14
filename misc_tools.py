e5f50f435994c6169c810f71741c896a04968327
John Hawkins <hawkjo@gmail.com>
2014-08-14 01:00:15 -0500

    misc_tools ssh restructuring
    Changed connection to happen during init.
    Added sftp_mkdir_p

--- misc_tools.py
+++ misc_tools.py
@@ -53,8 +53,8 @@ class ssh:
         self.username = username
         self.password = password
 
-    def __enter__(self):
         self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
         if self.username and self.password:
             self.ssh_client.connect( self.hostname, username=self.username, password=self.password )
         elif not self.username and not self.password:
@@ -66,8 +66,36 @@ class ssh:
         else:
             sys.exit( 'SSH error: Either use config/keys or username/password' )
         self.sftp_client = self.ssh_client.open_sftp()
+
+    def close(self):
+        self.sftp_client.close()
+        self.ssh_client.close()
+
+    def __enter__(self):
         return self
 
     def __exit__(self, etype, value, traceback):
-        self.sftp_client.close()
-        self.ssh_client.close()
+        self.close()
+
+    def sftp_mkdir_p(self, dir_path):
+        try:
+            self.sftp_client
+        except:
+            sys.exit( 'sftp_client not defined. Check docstring for proper usage.' )
+
+        if not dir_path: return
+
+        self.sftp_client.chdir( '.' )
+        starting_dir = self.sftp_client.getcwd()
+
+        folders = dir_path.split('/')
+        if folders[0] == '':
+            self.sftp_client.chdir( '/' )
+            folders = folders[1:]
+        for folder in folders:
+            try:
+                self.sftp_client.chdir( folder )
+            except IOError:
+                self.sftp_client.mkdir( folder )
+                self.sftp_client.chdir( folder )
+        self.sftp_client.chdir( starting_dir )
