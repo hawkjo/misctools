fca020e1b59673a47f442520ed08abde72b80a12
John Hawkins <hawkjo@gmail.com>
2014-11-03 19:01:22 -0600

    misc_tools.py -> misctools.py pep8 compliant

--- misctools.py
+++ misctools.py
@@ -1,6 +1,10 @@
-import sys, os, gzip, paramiko, re
+import sys
+import os
+import gzip
+import paramiko
 
-def get_gzip_friendly_open_fnc( *args ):
+
+def get_gzip_friendly_open_fnc(*args):
     """
     get_open_fnc returns the correct open function to use for file, gzip compatible.
     Arguments:
@@ -8,16 +12,18 @@ def get_gzip_friendly_open_fnc( *args ):
     Returns:
         The open function to use: either 'open' or 'gzip.open'
     """
-    fname_extensions = set([os.path.splitext( fname )[-1] for fname in args])
+    fname_extensions = set([os.path.splitext(fname)[-1] for fname in args])
     if '.gz' not in fname_extensions:
         return open
     elif fname_extensions == set(['.gz']):
         import gzip
         return gzip.open
     else:
-        sys.exit('Error: All files must be of the same format and either fastq or gzipped fastq files.')
+        sys.exit(
+            'Error: All files must be of the same format and either fastq or gzipped fastq files.')
 
-def gzip_friendly_open( fname, mode='r' ):
+
+def gzip_friendly_open(fname, mode='r'):
     """
     gzip_friendly_open returns a file handle appropriate for the file, whether gzipped or not.
     """
@@ -25,6 +31,7 @@ def gzip_friendly_open( fname, mode='r' ):
         return gzip.open(fname, mode)
     else:
         return open(fname, mode)
+
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -37,6 +44,7 @@ class cd:
 
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
+
 
 class ssh:
     """
@@ -54,17 +62,19 @@ class ssh:
         self.password = password
 
         self.ssh_client = paramiko.SSHClient()
-        self.ssh_client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         if self.username and self.password:
-            self.ssh_client.connect( self.hostname, username=self.username, password=self.password )
+            self.ssh_client.connect(self.hostname, username=self.username, password=self.password)
         elif not self.username and not self.password:
             config = paramiko.SSHConfig()
-            config.parse( open('/home/hawkjo/.ssh/config') )
-            d = config.lookup( self.hostname )
+            config.parse(open('/home/hawkjo/.ssh/config'))
+            d = config.lookup(self.hostname)
             self.ssh_client.load_system_host_keys()
-            self.ssh_client.connect( d['hostname'], username=d['user'], key_filename=d['identityfile'] )
+            self.ssh_client.connect(d['hostname'],
+                                    username=d['user'],
+                                    key_filename=d['identityfile'])
         else:
-            sys.exit( 'SSH error: Either use config/keys or username/password' )
+            sys.exit('SSH error: Either use config/keys or username/password')
         self.sftp_client = self.ssh_client.open_sftp()
 
     def close(self):
@@ -81,21 +91,22 @@ class ssh:
         try:
             self.sftp_client
         except:
-            sys.exit( 'sftp_client not defined. Check docstring for proper usage.' )
+            sys.exit('sftp_client not defined. Check docstring for proper usage.')
 
-        if not dir_path: return
+        if not dir_path:
+            return
 
-        self.sftp_client.chdir( '.' )
+        self.sftp_client.chdir('.')
         starting_dir = self.sftp_client.getcwd()
 
         folders = dir_path.split('/')
         if folders[0] == '':
-            self.sftp_client.chdir( '/' )
+            self.sftp_client.chdir('/')
             folders = folders[1:]
         for folder in folders:
             try:
-                self.sftp_client.chdir( folder )
+                self.sftp_client.chdir(folder)
             except IOError:
-                self.sftp_client.mkdir( folder )
-                self.sftp_client.chdir( folder )
-        self.sftp_client.chdir( starting_dir )
+                self.sftp_client.mkdir(folder)
+                self.sftp_client.chdir(folder)
+        self.sftp_client.chdir(starting_dir)
